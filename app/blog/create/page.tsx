489f7956c5ab7f7298d7591c56c27e4ba8282fe9
Turmunkh <turmunkhn@nestsolutions.llc>
2023-12-21 18:06:37 -0800

    Fixed the create page

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,152 +1,135 @@
+"use client";
+import { app } from "@/app/firebase";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { ChangeEventHandler, useState } from "react";
 
-"use client"
-import { Button } from '@mui/material';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { addDoc, collection } from 'firebase/firestore';
-import { useState } from 'react';
-import { getFirestore } from "firebase/firestore";
-import { app } from '@/app/firebase';
-import { styled } from '@mui/material/styles';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-
-
-
-
-const db = getFirestore(app);
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 export default function Home() {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [firstimage, setFirstimage] = useState();
-    const [avatarimage, setAvatarimage] = useState();
-    const [personname, setPersonname] = useState("");
-    const [date, setDate] = useState("");
-    const [loading, setLoading] = useState(false);
-    const firestore = getFirestore(app);
-    const storage = getStorage(app);
-    
-    const VisuallyHiddenInput = styled('input')({
-        clip: 'rect(0 0 0 0)',
-        clipPath: 'inset(50%)',
-        height: 1,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        whiteSpace: 'nowrap',
-        width: 1,
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [firstimage, setFirstimage] = useState<File>();
+  const [avatarimage, setAvatarimage] = useState<File>();
+  const [personname, setPersonname] = useState("");
+  const [date, setDate] = useState("");
+  const [loading, setLoading] = useState(false);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+
+  const handleSaveToFirestoreButton = async () => {
+    setLoading(true);
+    const storageRef = ref(storage, firstimage?.name);
+    const avatarStorageRef = ref(storage, avatarimage?.name);
+    await uploadBytes(storageRef, firstimage as Blob);
+    await uploadBytes(avatarStorageRef, avatarimage as Blob);
+    const urlfirstimage = await getDownloadURL(storageRef);
+    const urlAvatarImage = await getDownloadURL(avatarStorageRef);
+
+    await addDoc(collection(db, "blog"), {
+      title: title,
+      description: description,
+      firstimage: urlfirstimage,
+      avatarimage: urlAvatarImage,
+      personname: personname,
+      date: date,
     });
+    setLoading(false);
+  };
 
-    return (
-        <main>
-            <h1>вэб хуудаснаас датаг firebase-д хэрхэн оруулах</h1>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "10px",
-                    paddingLeft: "10px"
-                }}>
-                <p>
-                    {JSON.stringify({
-                        title: title,
-                        description: description,
-                        personname: personname,
-                        date: date,
+  const handleTitleChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    setTitle(e.target.value);
+  };
 
-                    })}
-                </p>
-                <Box
-                    component="form"
-                    sx={{
-                        '& > :not(style)': { m: 1, width: '25ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField id="outlined-basic" label="Title" variant="outlined"
-                        onChange={e => { setTitle(e.target.value); }}
-                    />
-                    <TextField id="filled-basic" label="Description" variant="filled"
-                        onChange={e => { setDescription(e.target.value); }}
-                    />
-                    <TextField id="outlined-basic" label="Name" variant="outlined"
-                        onChange={e => { setPersonname(e.target.value); }}
-                    />
-                    <TextField id="filled-basic" label="Date" variant="filled"
-                        onChange={e => { setDate(e.target.value); }}
-                    />
-                    <div
-                        style={{
-                            display: "flex", gap: "20px"
-                        }}>
-                        <div>
-                            <Button component="label" variant="contained">
-                                Choose firstImage file
-                                <VisuallyHiddenInput type="file" onChange={(e) => { setFirstimage(e.target.files[0]); }} />
-                            </Button>
-                        </div>
-                        <div>
-                            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}
-                                onClick={async () => {
-                                    const storageRef = ref(storage, firstimage?.name);
-                                    await uploadBytes(storageRef, firstimage).then((snapshot) => {
-                                        console.log("Upload a blob or a file");
-                                    })
-                                    const urlfirstimage = await getDownloadURL(storageRef)
-                                    console.log(urlfirstimage)
-                                    await addDoc(collection(firestore, "blog"),{urlfirstimage,});
-                                }}>Upload firstImage
-                            </Button>
-                        </div>
-
-                        <div>
-                            <Button component="label" variant="contained" >
-                                Choose avatarimage file
-                                <VisuallyHiddenInput type="file" onChange={(e) => { setAvatarimage(e.target.files[0]); }} />
-                            </Button>
-                        </div>
-                        <div>
-                            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}
-                                onClick={async () => {
-                                    
-                                }}>Upload avatarimage
-                            </Button>
-                        </div>
-
-                    </div>
-                    <div>
-
-                        {/* <input type="file" onChange={handleFileChange}/>
-                        <Button onClick={handleButtonClick}>Upload to storage</Button>
-                        <Button onClick={handleCreateFirestoreDocument}>Create firestore document</Button> */}
-                        
-                        <Button variant="contained" onClick={async () => {
-                            await addDoc(collection(db, "blog"), {
-                                title: title,
-                                description: description,
-                                firstimage: firstimage,
-                                avatarimage: avatarimage,
-                                personname: personname,
-                                date: date,
-                            });
-                            await addDoc(collection(firestore, "blog"), {
-                              
-
-                            });
-                            setLoading
-                        }}>{loading ?
-                            <div>
-                                Loading...
-                            </div> : <div>Save to firestore</div>}
-
-                        </Button>
-                    </div>
-                </Box>
-
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <h1>вэб хуудаснаас датаг firebase-д хэрхэн оруулах</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          paddingLeft: "10px",
+        }}
+      >
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            label="Title"
+            variant="outlined"
+            onChange={handleTitleChange}
+          />
+          <TextField
+            label="Description"
+            variant="filled"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+            onChange={(e) => {
+              setPersonname(e.target.value);
+            }}
+          />
+          <TextField
+            id="filled-basic"
+            label="Date"
+            variant="filled"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+          <Button component="label" variant="contained">
+            Choose First Image file
+            <VisuallyHiddenInput
+              type="file"
+              onChange={(e) => {
+                setFirstimage(e.target.files[0]);
+              }}
+            />
+          </Button>
+          <Button component="label" variant="contained">
+            Choose Avatar Image file
+            <VisuallyHiddenInput
+              type="file"
+              onChange={(e) => {
+                setAvatarimage(e.target.files[0]);
+              }}
+            />
+          </Button>
+          <div>
+            <Button variant="contained" onClick={handleSaveToFirestoreButton}>
+              {loading ? <div>Loading...</div> : <div>Save to firestore</div>}
+            </Button>
+          </div>
+        </Box>
+      </div>
+    </main>
+  );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,80 +1,71 @@
-"use client"
+"use client";
+import { collection, getFirestore } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { NavigationBar } from "../components/navigation-bar";
 import { app } from "../firebase";
 import { Blogitem } from "./blog-item";
 
-import { collection, getFirestore } from 'firebase/firestore';
-import { useCollection } from 'react-firebase-hooks/firestore';
-
-
-
-
 export default function Home() {
-    const [value, loading, error] = useCollection(
-        collection(getFirestore(app), 'blog'),
-        {
-            snapshotListenOptions: { includeMetadataChanges: true },
-        }
-    );
-    return (
-        <div style={{ backgroundColor: "lightgrey" }}>
-            <NavigationBar />
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                <p
-                    style={{
-                        fontSize: "48px",
-                        width: "246px",
-                        height: "60px",
-                        top: "160px",
-                        left: "140px"
-                    }}
-                >
-                    Blog posts
-                </p>
-                <p
-                    style={{
-                        fontSize: "18px",
-                        width: "480px",
-                        height: "29px",
-                        top: "240px",
-                        left: "140px",
-                        color: "rgba(109, 125, 139, 1)"
-
-                    }}
-                >
-                    Our latest updates and blogs about managing your team
-                </p>
-            </div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                <main
-                    style={{
-                        height: "100vh",
-                        width: "100vw",
-                        display: "flex",
-                        flexWrap: "wrap",
-                        backgroundColor: "#d3d3d3",
-                        gap: "20px",
-                        
-                    }}
-                >
-                    {value &&
-                        value.docs.map((doc) => (
-                            <div style={{ display: "flex" }}
-                                key={doc.id}>
-                                <Blogitem
-                                    title={doc.data().title}
-                                    description={doc.data().description}
-                                    firstimage={doc.data().firstimage}
-                                    avatarimage={doc.data().avatarimage}
-                                    name={doc.data().name}
-                                    date={doc.data().date}
-                                />
-
-                            </div>
-                        ))
-                    }
-                </main>
-            </div>
-        </div >
-    );
+  const [value, loading, error] = useCollection(
+    collection(getFirestore(app), "blog"),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+  return (
+    <div style={{ backgroundColor: "lightgrey" }}>
+      <NavigationBar />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <p
+          style={{
+            fontSize: "48px",
+            width: "246px",
+            height: "60px",
+            top: "160px",
+            left: "140px",
+          }}
+        >
+          Blog posts
+        </p>
+        <p
+          style={{
+            fontSize: "18px",
+            width: "480px",
+            height: "29px",
+            top: "240px",
+            left: "140px",
+            color: "rgba(109, 125, 139, 1)",
+          }}
+        >
+          Our latest updates and blogs about managing your team
+        </p>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <main
+          style={{
+            height: "100vh",
+            width: "100vw",
+            display: "flex",
+            flexWrap: "wrap",
+            backgroundColor: "#d3d3d3",
+            gap: "20px",
+          }}
+        >
+          {value &&
+            value.docs.map((doc) => (
+              <div style={{ display: "flex" }} key={doc.id}>
+                <Blogitem
+                  title={doc.data().title}
+                  description={doc.data().description}
+                  firstimage={doc.data().firstimage}
+                  avatarimage={doc.data().avatarimage}
+                  name={doc.data().name}
+                  date={doc.data().date}
+                />
+              </div>
+            ))}
+        </main>
+      </div>
+    </div>
+  );
 }
