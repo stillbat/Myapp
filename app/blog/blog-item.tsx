2c237b7ffccf320e0ed584982e8e1c8e2d838767
Turmunkh <turmunkhn@nestsolutions.llc>
2024-01-05 09:31:42 -0800

    Fixed the first couple of typescript errors

--- app/blog/blog-item.tsx
+++ app/blog/blog-item.tsx
@@ -1,46 +1,61 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export const Blogitem = ({title, description, firstimage, avatarimage, name, date }) => {
-    
-    return (
-        <div style={{ display: "flex", gap: "50px" }}>
-            <div
-                style={{
-                    height: "440px",
-                    width: "363px",
-                    backgroundColor: "white",
-                    borderRadius: "30px",
-                    overflow: "hidden",
-                    gap: "50px"
-                }}>
-                <Image src={firstimage} width={370} height={147} alt="" />
-                <div
-                    style={{
-                        paddingLeft: "24px",
-                        paddingBottom: "19px",
-                        paddingTop: "10px",
-                    }}
-                >
-                    <p style={{ fontSize: "24px", width: "330px", height: "58px" }}>
-                        {title}
-                    </p>
+export const Blogitem = ({
+  title,
+  description,
+  firstimage,
+  avatarimage,
+  name,
+  date,
+}: {
+  title: string;
+  description: string;
+  firstimage: string;
+  avatarimage: string;
+  name: string;
+  date: string;
+}) => {
+  return (
+    <div style={{ display: "flex", gap: "50px" }}>
+      <div
+        style={{
+          height: "440px",
+          width: "363px",
+          backgroundColor: "white",
+          borderRadius: "30px",
+          overflow: "hidden",
+          gap: "50px",
+        }}
+      >
+        <Image src={firstimage} width={370} height={147} alt="" />
+        <div
+          style={{
+            paddingLeft: "24px",
+            paddingBottom: "19px",
+            paddingTop: "10px",
+          }}
+        >
+          <p style={{ fontSize: "24px", width: "330px", height: "58px" }}>
+            {title}
+          </p>
 
-                    <p style={{ paddingTop: "25px", paddingBottom: "57px" }}>
-                        {description}
-                    </p>
-                    <div
-                        style={{
-                            display: "flex",
-                            gap: "10px",
-                            alignItems: "center",
-                        }} >
-                        <Image src={avatarimage} width={45} height={45} alt="" />
-                        <p>{name}</p>
-                        <p>|</p>
-                        <p>{date}</p>
-                    </div>
-                </div>
-            </div>
+          <p style={{ paddingTop: "25px", paddingBottom: "57px" }}>
+            {description}
+          </p>
+          <div
+            style={{
+              display: "flex",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            <Image src={avatarimage} width={45} height={45} alt="" />
+            <p>{name}</p>
+            <p>|</p>
+            <p>{date}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
             ))}
         </main>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
